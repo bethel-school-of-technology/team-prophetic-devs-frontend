7cf88f45f4ffc54b918162dbe82ebe36c88e371d
Josiah Sanchez <josiah.rasanchez@gmail.com>
2021-01-19 12:34:56 -0700

    Added a lot. Created pages, and working routes with the front end. Intagrated some code from Palmers log in page. Take a look and see how you guys like it.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,32 +1,40 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AllMaterialComponentsModule } from './Angular Material Imports/all-material-components.module';
+//Routing Imports
+import { AppRoutingModule } from './app-routing.module';
 
+//Component Imports
 import { AppComponent } from './app.component';
-
-import {MatToolbarModule} from '@angular/material/toolbar';
-import {MatButtonModule} from '@angular/material/button';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
-import {MatGridListModule} from '@angular/material/grid-list';
-import {MatIconModule} from '@angular/material/icon';
-import {MatMenuModule} from '@angular/material/menu';
-import {MatDividerModule} from '@angular/material/divider';
+import { HomePageComponent } from './home-page/home-page.component';
+import { LoginPageComponent } from './login-page/login-page.component';
+import { ProfilePageComponent } from './profile-page/profile-page.component';
+import { ChatWindowComponent } from './home-page/ChatWindowFolder/ChatWindowComponent/chat-window/ChatWindowFolder/chat-window.component';
+import { RegisterComponent } from './login-page/register/register.component';
+import { LogInComponent } from './login-page/log-in/log-in.component';
+
+
+
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavBarComponent
+    NavBarComponent,
+    HomePageComponent,
+    LoginPageComponent,
+    ProfilePageComponent,
+    ChatWindowComponent,
+    RegisterComponent,
+    LogInComponent
   ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
-    MatToolbarModule,
-    MatButtonModule,
-    MatGridListModule,
-    MatIconModule,
-    MatMenuModule,
-    MatDividerModule
+    AppRoutingModule,
+    AllMaterialComponentsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
