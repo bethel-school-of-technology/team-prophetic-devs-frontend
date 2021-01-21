32348bcfb193b2b91df920b8fa9f95529dc37eb2
Josiah Sanchez <josiah.rasanchez@gmail.com>
2021-01-20 23:31:59 -0700

    Begining of the Home Page!!!

--- src/app/home-page/ChatWindowFolder/ChatWindowComponent/chat-window/ChatWindowFolder/chat-window.component.ts
+++ src/app/home-page/ChatWindowFolder/ChatWindowComponent/chat-window/ChatWindowFolder/chat-window.component.ts
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./chat-window.component.css']
 })
 export class ChatWindowComponent implements OnInit {
-
+  value = 'Message...'
   constructor() { }
 
   ngOnInit(): void {
